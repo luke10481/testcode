83a1900d6646ab7d13f9e3c9328823249b568ff3
LuJianHao <594830589@qq.com>
2023-02-20 01:52:51 +0800

    1

---
+++ BroadcastReceiverDemo/app/src/main/java/com/example/myapplication/DynamicBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class DynamicBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "收到动态注册广播", Toast.LENGTH_SHORT).show();
+
+    }
+}
